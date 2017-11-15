2d0c1f41fdbdd2d0bbb97dd765cd052b6d395ddd
Toshca <toshcaw.proscrib@gmail.com>
2017-11-14 19:27:01 -0500

    Update tab.js

--- js/tab.js
+++ js/tab.js
@@ -3,43 +3,16 @@
 // Make the entire body a menu tabbed page, in other words make all of the pages live on the home page. Thank you W3Schools for some of the guidance here.  And entheosweb.com for responsive guidance.
 
  // tabbed content
-     // tabbed content
-    // http://www.entheosweb.com/tutorials/css/tabs.asp
-    $(".tab_content").hide();
-    $(".tab_content:first").show();
-
-  /* if in tab mode */
-    $("ul.tabs li").click(function() {
-		
-      $(".tab_content").hide();
-      var activeTab = $(this).attr("rel"); 
-      $("#"+activeTab).fadeIn();		
-		
-      $("ul.tabs li").removeClass("active");
-      $(this).addClass("active");
-
-	  $(".tab_drawer_heading").removeClass("d_active");
-	  $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
-	  
-    });
-	/* if in drawer mode */
-	$(".tab_drawer_heading").click(function() {
-      
-      $(".tab_content").hide();
-      var d_activeTab = $(this).attr("rel"); 
-      $("#"+d_activeTab).fadeIn();
-	  
-	  $(".tab_drawer_heading").removeClass("d_active");
-      $(this).addClass("d_active");
-	  
-	  $("ul.tabs li").removeClass("active");
-	  $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
-    });
-	
-	
-	/* Extra class "tab_last" 
-	   to add border to right side
-	   of last tab */
-	$('ul.tabs li').last().addClass("tab_last");
-	
-	
+function openCity(evt, archiveName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("archive");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab-link");
+  for (i = 0; i < x.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace(" tab-border-lime", "");
+  }
+  document.getElementById(archiveName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " tab-border-lime";
+}
